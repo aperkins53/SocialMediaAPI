aae055f7263a0292738ba1ddc24d6a394b5abbc7
Justin <Justin@DESKTOP-UOG00UE>
2020-04-26 23:07:05 -0400

    Completed CRUD for Post and Comment Classes

---
+++ SocialMediaAPI.Services/PostService.cs
@@ -0,0 +1,127 @@
+﻿using Data;
+using SocialMediaAPI.Data;
+using SocialMediaAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaAPI.Services
+{
+    public class PostService
+    {
+        private readonly Guid _userId;
+        public PostService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreatePost(PostCreate model)
+        {
+            var entity =
+                new Post()
+                {
+                    OwnerId = _userId,
+                    Title = model.Title,
+                    PostContent = model.Content,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Posts.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<PostListItem> GetPost()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Posts
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                            e =>
+                                new PostListItem
+                                {
+                                    PostId = e.PostId,
+                                    Title = e.Title,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+
+        public PostDetail GetPostById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Posts
+                        .Single(e => e.PostId == id && e.OwnerId == _userId);
+                return
+                    new PostDetail
+                    {
+                        PostId = entity.PostId,
+                        Title = entity.Title,
+                        Content = entity.Content,
+                        CreatedUtc = entity.CreatedUtc,
+                        ModifiedUtc = entity.ModifiedUtc
+                    };
+            }
+        }
+
+        public bool UpdatePost(PostEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Posts
+                        .Single(e => e.PostId == model.PostId && e.OwnerId == _userId);
+
+                entity.Title = model.Title;
+                entity.Content = model.Content;
+                entity.ModifiedUtc = DateTimeOffset.UtcNow;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeletePost(int postId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Posts
+                        .Single(e => e.PostId == postId && e.OwnerId == _userId);
+
+                ctx.Posts.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteComment(int commentId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Comments
+                        .Single(e => e.CommentId == commentId && e.OwnerId == _userId);
+
+                ctx.Comments.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
--- TeamCAJESocialMediaAPI/Controllers/PostController.cs
+++ TeamCAJESocialMediaAPI/Controllers/PostController.cs
@@ -8,34 +8,64 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace TeamCAJESocialMediaAPI.Controllers
+namespace SocialMediaAPI.Controllers
 {
     public class PostController : ApiController
-    { 
-            public IHttpActionResult Get()
-            {
-                PostServices postService = CreatePostService();
-            var notes = postService.GetPost();
-                return Ok(notes);
-            }
-            public IHttpActionResult Post(PostCreate post)
-            {
-                if (!ModelState.IsValid)
-                    return BadRequest(ModelState);
+    {
+        public IHttpActionResult Get()
+        {
+            PostService postService = CreatePostService();
+            var posts = postService.GetPost();
+            return Ok(posts);
+        }
+        public IHttpActionResult Post(PostCreate post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-                var service = CreatePostService();
+            var service = CreatePostService();
 
-                if (!service.CreatePost(post))
-                    return InternalServerError();
+            if (!service.CreatePost(post))
+                return InternalServerError();
 
-                return Ok();
-            }
-            private PostServices CreatePostService()
-            {
-                var userId = Guid.Parse(User.Identity.GetUserId());
-                var noteService = new PostServices(userId);
-                return noteService;
-            }
+            return Ok();
+        }
+        private PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var postService = new PostService(userId);
+            return postService;
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            PostService noteService = CreatePostService();
+            var note = noteService.GetPostById(id);
+            return Ok(note);
+        }
+
+        public IHttpActionResult Put(PostEdit post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePostService();
+
+            if (!service.UpdatePost(post))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreatePostService();
+
+            if (!service.DeletePost(id))
+                return InternalServerError();
+
+            return Ok();
         }
     }
+}
 
