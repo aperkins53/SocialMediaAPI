3ead0ef26e208eeed2080bc8ceaef179c6baac13
Emina Palamarevic <minapala342@gmail.com>
2020-04-28 18:18:23 -0400

    updated Reply

---
+++ Data/Reply.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data
+{
+    public class Reply 
+    {
+        [Key]
+        public bool isReply { get; set; }
+        public string Content { get; set; }
+        public int ReplyId { get; set; }
+
+        [ForeignKey("Comment")]
+        public int CommentId { get; set; }
+        public virtual Comment Comment { get; set; }
+    }
+}
